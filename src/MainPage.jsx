35e0e23164e1cad04cb9899b976e87510d0c8156
Hitansh Paliwal <hitansh98@yahoo.com>
2023-05-09 01:11:01 +0530

    Started learning Project

---
+++ src/MainPage.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function MainPage() {
+  return (
+    <>
+      <div>
+        <div class = "container">
+            <div className = "item item-1">January</div>
+            <div className = "item item-2">February</div>
+            <div className = "item item-3">March</div>
+            <div className = "item item-4">April</div>
+            <div className = "item item-5">May</div>
+            <div className = "item item-6">June</div>
+            <div className = "item item-7">July</div>
+            <div className = "item item-8">August</div>
+            <div className = "item item-9">September</div>
+            <div className = "item item-10">October</div>
+            <div className = "item item-11">November</div>
+            <div className = "item item-12">December</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default MainPage
